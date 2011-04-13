964c37417ffd3c7fb1cf25c9ebe68e8147c5fcd1
Francesco Lazzarino <flazzarino@gmail.com>
2011-04-13 11:47:31 -0400

    nice colors

--- Main.hs
+++ Main.hs
@@ -6,12 +6,19 @@ import Data.IORef
 import Data.Array.IArray
 import Conway
 
+base03 = Color4 0 (43/255)  (54/255) 1 :: Color4 GLclampf
+base02 = Color4 (7/255) (54/255) (66/255) 1 :: Color4 GLclampf
+magenta = Color4 (211/255) (54/255) (130/255) 1 :: Color4 GLclampf
+violet = Color4 (108/255) (113/255) (196/255) 1 :: Color4 GLclampf
+blue = Color4 (38/255) (139/255) (210/255) 1 :: Color4 GLclampf
+green = Color4 (133/255) (153/255) (0/255) 1 :: Color4 GLclampf
+
 black = Color4 0 0 0 1 :: Color4 GLclampf
 white = Color4 1 1 1 1 :: Color4 GLclampf
-red = Color4 1 0 0 1 :: Color4 GLclampf
 
-dead = Color4 1 1 1 0.15 :: Color4 GLclampf
-blue = Color4 0 0 1 1 :: Color4 GLclampf
+period delay action = do
+  action
+  GLUT.addTimerCallback delay $ period delay action
 
 main :: IO ()
 main = do
@@ -32,32 +39,39 @@ main = do
   blend $= Enabled
   blendFunc $= (SrcAlpha, OneMinusSrcAlpha)
 
-  period 200 $ do
+  period 150 $ do
     modifyIORef gridRef update
     GLUT.postRedisplay Nothing
 
-  GLUT.mainLoop
+  clearColor $= base03
 
-period delay action = do
-  action
-  GLUT.addTimerCallback delay $ period delay action
+  GLUT.mainLoop
 
 drawCell :: (GridIx, Bool) -> IO ()
 drawCell ((col, row), state) = do
   let x = fromIntegral col
-  let y = fromIntegral row
-
-
-  let padding = 0.05
-  let v1 = Vertex2 (x + padding) (y + padding) :: Vertex2 GLfloat
-  let v2 = Vertex2 (x + 1 - padding) (y + 1 - padding) :: Vertex2 GLfloat
-
-  let c = case state of
-            True -> blue
-            otherwise -> dead
-  color c
-
+      y = fromIntegral row
+      v1 = Vertex2 x y :: Vertex2 GLfloat
+      v2 = Vertex2 (x + 1) (y + 1) :: Vertex2 GLfloat
   rect v1 v2
+
+hLine :: GLfloat -> GLfloat -> IO ()
+hLine w y = do
+  let v1 = Vertex2 1 y :: Vertex2 GLfloat
+  let v2 = Vertex2 w y :: Vertex2 GLfloat
+
+  renderPrimitive Lines $ do
+    vertex v1
+    vertex v2
+
+vLine :: GLfloat -> GLfloat -> IO ()
+vLine h x = do
+  let v1 = Vertex2 x 1 :: Vertex2 GLfloat
+  let v2 = Vertex2 x h :: Vertex2 GLfloat
+
+  renderPrimitive Lines $ do
+    vertex v1
+    vertex v2
 
 display :: IORef Grid -> IO ()
 display gridRef = do
@@ -78,6 +92,12 @@ display gridRef = do
     let origin = Vector3 (width/(-2) - 1) (height/(-2) - 1) 0 :: Vector3 GLfloat
     translate origin
 
-    mapM_ drawCell . assocs $ g
+    color green
+    let activeCell (_, state) = state
+    mapM_ drawCell . filter activeCell . assocs $ g
+
+    color base02
+    mapM_ ( hLine $ width + 1 ) [1..height]
+    mapM_ ( vLine $ height + 1 ) [1..width]
 
   GLUT.swapBuffers
